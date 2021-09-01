2e07c48ea84413bdcd38d5764c5685ab642483ff
FernandaSacoli <fersacoli@gmail.com>
2021-09-01 20:40:17 +0000

    commit aula 2

--- App.js
+++ App.js
@@ -1,71 +1,96 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator} from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
+import{ createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Inicial from './src/views/Inicial/Inicial';
 import Colecao from './src/views/Colecao/Colecao';
 import Item from './src/views/Item/Item';
+import Perfil from './src/views/Perfil/Perfil';
+
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function rotasStack() {
+
+  return(
+    <Stack.Navigator initialRouteName="Inicial">
+
+      <Stack.Screen 
+      name="Inicial"
+      component={Inicial}
+      options={
+        {cardStyle: styles.screenInicial,
+          headerStyle: {backgroundColor: '#14417b'},
+          headerTintColor: '#FFFFFF',
+        }
+      }
+
+      />
+      
+      <Stack.Screen 
+              name="Colecao"
+              component={Colecao}
+              options={
+                {cardStyle: styles.screenItem,
+                  headerStyle: {backgroundColor: '#14417b'},
+                  headerTintColor: '#FFFFFF',
+                }
+              }
+
+              />   
+        <Stack.Screen 
+              name="Item"
+              component={Item}
+              options={
+                {cardStyle: styles.screenItem,
+                  headerStyle: {backgroundColor: '#14417b'},
+                  headerTintColor: '#FFFFFF',
+                }
+              }
+
+              />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
-
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-        name="Inicial"
-        component={Inicial}
-        options={
-          { CardStyle: styles.screenInicial,
-          headerStyle: {backgroundColor: '#14417b'},
-          headerTintColor: '#FFFFFF',
-          }
+      <StatusBar barStyle="light-content" backgroundColor="#081a31" />
+      <Tab.Navigator
+      tabBarOptions={
+        {
+          labelStyle: {fontSize: 20},
+          style: {backgroundColor: '#14417b'},
+          activeTintColor: '#000000'
         }
-        />
-           <Stack.Screen
-        name="Colecao"
-        component={Colecao}
-        options={
-          { CardStyle: styles.screenColecao,
-          headerStyle: {backgroundColor: '#14417b'},
-          headerTintColor: '#FFFFFF',
-          }
-        }
-        />
-
-        <Stack.Screen
-          name="Item"
-          component={Item}
-          options={
-          { CardStyle: styles.screenItem,
-          headerStyle: {backgroundColor: '#14417b'},
-          headerTintColor: '#FFFFFF',
-         }
-       }
-       />
-      </Stack.Navigator>
-      
+      }
+      >
+        <Tab.Screen name="Inicial" component={rotasStack} />
+        <Tab.Screen name="Perfil" component={Perfil} />
+        </Tab.Navigator>
     </NavigationContainer>
-   
   );
-  
-  }
+}
 
-  const styles = StyleSheet.create({
-        screenInicial: {
-          backgroundColor: '#000000'
-        },
-        screenColecao: {
-          backgroundColor: '#000000'
-        },
-        screenItem: {
-          backgroundColor: '#000000'
-        },
-  });
+const styles = StyleSheet.create({
+  screenInicial: {
+        backgroundColor: '#92afd7'
+  },
+  screenColeaco: {
+      backgroundColor: '#d1d1d1'
+  },
+  screenItem: {
+    backgroundColor: "#d0f4ea"
+  },
 
 
+});
 
 
  
